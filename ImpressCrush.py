e27a2f1736e880ecf23f3f2681d25fe766eb09ea
John Rios <71901224+Joohnrios@users.noreply.github.com>
2022-11-22 17:36:03 -0300

    First commit

---
+++ ImpressCrush.py
@@ -0,0 +1,22 @@
+from turtle import *
+from time import sleep
+
+
+color('red')
+begin_fill()
+pensize(3)
+sleep(1)
+left(50)
+sleep(1)
+forward(133)
+sleep(1)
+circle(50, 200)
+sleep(1)
+right(140)
+sleep(1)
+circle(50, 200)
+sleep(1)
+forward(133)
+
+sleep(5)
+end_fill
