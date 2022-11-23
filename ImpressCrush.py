7c3fd2b4f72ceb04951cc1df4a82515a00b995e0
John Rios <71901224+Joohnrios@users.noreply.github.com>
2022-11-23 07:54:19 -0300

    Updating with your crush name...

--- ImpressCrush.py
+++ ImpressCrush.py
@@ -1,16 +1,26 @@
 from turtle import *
 from time import sleep
+import turtle
 
 
 color('red')
 begin_fill()
-pensize(3)
+
+
+turtle.write('Your crush!', move=False, font=(
+    'Comic Sans', 18, 'bold'), align='center')
+
+
+turtle.sety(-90)
+
+pensize(8)
 sleep(1)
 left(50)
 sleep(1)
 forward(133)
 sleep(1)
 circle(50, 200)
+
 sleep(1)
 right(140)
 sleep(1)
