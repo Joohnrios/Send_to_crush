1f918d38a12172d0aeaa2941cf3cfa4c6e982494
John Rios <71901224+Joohnrios@users.noreply.github.com>
2022-11-23 09:53:36 -0300

    Update no sleep 2.0

--- ImpressCrush.py
+++ ImpressCrush.py
@@ -14,18 +14,11 @@ turtle.write('Your crush!', move=False, font=(
 turtle.sety(-90)
 
 pensize(8)
-sleep(1)
 left(50)
-sleep(1)
 forward(133)
-sleep(1)
 circle(50, 200)
-
-sleep(1)
 right(140)
-sleep(1)
 circle(50, 200)
-sleep(1)
 forward(133)
 
 sleep(5)
